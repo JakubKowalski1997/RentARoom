41f9c3fc9e39c3667734623c193f7692c2d489f7
Jakub Kowalski <kubabartek1@op.pl>
2020-04-04 18:37:17 +0200

    Initial commit

---
+++ src/main/java/com/edu/agh/fis/RentARoom/RentARoomApplication.java
@@ -0,0 +1,13 @@
+package com.edu.agh.fis.RentARoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentARoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentARoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/agh/fis/RentARoom/RentARoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.agh.fis.RentARoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentARoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
