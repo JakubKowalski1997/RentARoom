8efd9048744fc5661b9f987703473678c86bb99d
Jakub Kowalski <kubabartek1@op.pl>
2020-04-04 19:02:56 +0200

    init commit

---
+++ src/main/webapp/src/resources/layouts/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { HashRouter as Router } from "react-router-dom";
+import "../styles/App.css";
+// import Header from "./Header";
+// import Navigation from "./Navigation";
+// import UserBlock from "./UserBlock";
+// import MainPage from "./MainPage";
+// import Footer from "./Footer";
+
+class App extends Component {
+  render() {
+    return (
+        <html>
+        <header><title>This is title</title></header>
+        <body>
+        Hello world
+        </body>
+        </html>
+    );
+  }
+}
+
+export default App;
